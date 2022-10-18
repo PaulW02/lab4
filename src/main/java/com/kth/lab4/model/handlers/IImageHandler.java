c42e5b4e9f9d5312bbc4f623b03b46585a7e6b28
PaulW02 <pslwi@ug.kth.se>
2022-10-18 14:16:58 +0200

    Small fixes

--- src/main/java/com/kth/lab4/model/handlers/IImageHandler.java
+++ src/main/java/com/kth/lab4/model/handlers/IImageHandler.java
@@ -2,6 +2,10 @@ package com.kth.lab4.model.handlers;
 
 public interface IImageHandler {
 
+    /**
+     * @param originalImg a 2D matrix of the original image
+     * @return a new processed 2D matrix
+     */
     public abstract int[][] processImage(int[][] originalImg);
 
 }
